e06803598e7e19e751be9be6bba368fabb73977e
Kai Davenport <kaiyadavenport@gmail.com>
2017-07-17 22:05:24 +0100

    flag for disabling registry push

--- cmd/datamesh-server/rebuild.sh
+++ cmd/datamesh-server/rebuild.sh
@@ -7,4 +7,8 @@ docker cp datamesh-builder:/target/docker target/
 docker cp datamesh-builder:/target/datamesh-server target/
 docker rm -f datamesh-builder
 docker build -t $(hostname).local:80/lukemarsden/datamesh-server:pushpull .
-docker push $(hostname).local:80/lukemarsden/datamesh-server:pushpull
+
+# allow disabling of registry push
+if [ -z "${NO_PUSH}" ]; then
+  docker push $(hostname).local:80/lukemarsden/datamesh-server:pushpull
+fi
