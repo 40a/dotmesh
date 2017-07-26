35477ff5d76a457af4ac76e1c3551b7f0486a851
Kai Davenport <kaiyadavenport@gmail.com>
2017-07-26 22:35:58 +0100

    configure api proxy

--- frontend/apps.config.js
+++ frontend/apps.config.js
@@ -3,13 +3,10 @@ const CONFIG = {
     "name": "ui",
     "title": "Datamesh"
   }],
-  sharedModules: [
-    
-  ],
-  linkedModules: [
-    'template-tools',
-    'template-ui'
-  ]
+  apiServers: [{
+    host: process.env.API_SERVICE_HOST || 'datamesh-server',
+    port: process.env.API_SERVICE_PORT || 6969
+  }]
 }
 
 module.exports = CONFIG
