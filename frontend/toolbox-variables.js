e0588922f82f5bb45a6a3450c907f98e849d5baf
Kai Davenport <kaiyadavenport@gmail.com>
2017-07-27 16:46:36 +0100

    use logo colors for theme

--- frontend/toolbox-variables.js
+++ frontend/toolbox-variables.js
@@ -1,5 +1,16 @@
+/*
+
+  orange = ff5722
+  pink = e91e63
+  yellow = ff9800
+  
+*/
+
+const orange = '#ff5722'
+const pink = '#e91e63'
+const yellow = '#ff9800'
+
 module.exports = {
-  'color-primary': 'var(--palette-blue-500)',
-  'color-primary-dark': 'var(--palette-blue-700)',
-  'color-primary-light': 'var(--palette-blue-300)'
+  'color-primary': pink,
+  'color-accent': orange,
 }
