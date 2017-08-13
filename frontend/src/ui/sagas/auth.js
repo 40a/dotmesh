1be83b3659da0361ee13b5643d44345021e86d6b
Kai Davenport <kaiyadavenport@gmail.com>
2017-08-13 15:03:42 +0100

    auth saga does not need driver

---
+++ frontend/src/ui/sagas/auth.js
@@ -0,0 +1,209 @@
+import { take, put, call, fork, select, all, takeLatest, takeEvery } from 'redux-saga/effects'
+
+import LocalStorage from '../../shared/utils/localStorage'
+
+import config from '../config'
+import forms from '../forms'
+import * as actions from '../actions'
+import * as selectors from '../selectors'
+
+import tools from '../tools'
+
+const REQUIRED_APIS = [
+  'login',
+  'register',
+  'status'
+]
+
+const AuthSagas = (opts = {}) => {
+  if(!opts.apis) throw new Error('auth saga requires a api option')
+  const apis = opts.apis
+  REQUIRED_APIS.forEach(name => {
+    if(!apis[name]) throw new Error(`${name} api required`)
+  })
+
+  ///////////////////////////////////////
+  ///////////////////////////////////////
+  // redux state
+
+  // write the given credentials to the value reducer
+  function* reduceCredentials(credentials) {
+    yield put(actions.auth.setUser(credentials))
+    return credentials
+  }
+
+  // get the current credentials from the value reducer
+  function* selectCredentials() {
+    const credentials = yield select(selectors.auth.user)
+    return credentials
+  }
+
+  // load credentials saved in local storage (because 'remember me')
+  const loadCachedCredentials = () => {
+    const localValue = LocalStorage.load(config.userLocalStorageName, true)
+    if(localValue) {
+      tools.logger(`found locally cached credentials`)
+      console.dir(localValue)
+    }
+    return localValue && localValue.username ?
+      localValue :
+      null
+  }
+
+  ///////////////////////////////////////
+  ///////////////////////////////////////
+  // local storage state
+
+  // save the given credentials to local storage (because 'remember me')
+  const saveCachedCredentials = (credentials) => LocalStorage.save(config.userLocalStorageName, credentials, true)
+  const deleteCachedCredentials = () => LocalStorage.del(config.userLocalStorageName)
+
+  ///////////////////////////////////////
+  ///////////////////////////////////////
+  // HOOKS
+
+  // if we have saved credentials (because 'remember me')
+  // then use them to login
+  function* initialize() {
+    const cachedCredentials = loadCachedCredentials()
+    if(cachedCredentials) {
+      yield call(login, cachedCredentials)
+    }
+  }
+
+  // given some credentials - login
+  function* login(credentials) {
+    if(!credentials) throw new Error('credentials required for login saga')
+
+    // remember is a local field - we don't want to send it to the backend api
+    const useCreds = Object.assign({}, credentials)
+    const remember = useCreds.remember
+    delete(useCreds.remember)
+
+    // run the login api
+    const { user, error } = yield call(apis.login, {
+      credentials
+    })
+
+    if(error) {
+      yield put(actions.router.hook('authLoginError', 'incorrect details'))
+      return
+    }
+    else {
+      // save the credentials to the store so until close/refresh we are authed
+      yield call(reduceCredentials, credentials)
+
+      if(remember) {
+        saveCachedCredentials(credentials)
+      }
+
+      // save the credentials to local storage so upon re-opening browser we are authenticated
+      yield put(actions.router.hook('authLoginSuccess', user))
+      return user
+    }
+  }
+
+  // called when the user clicks the submit button
+  // grab the values from the 'authLogin' form and send them to the 'login' saga
+  function* loginSubmit() {
+    const credentials = yield call(formValuesIfValid, 'authLogin')
+    yield call(login, credentials)
+  }
+
+  function* register() {
+
+    /*
+    const valid = yield select(isValid('register'))
+    if(!valid) return
+    const values = yield select(getFormValues('register'))
+
+    const { answer, error } = yield call(apiSaga, {
+      name: 'authRegister',
+      actions: actions.register,
+      api: apis.register,
+      payload: values
+    })
+
+    if(error) {
+      yield put(routerActions.hook('authRegisterError', user))
+      return
+    }
+
+    const user = yield call(status)
+    yield put(routerActions.hook('authRegisterSuccess', user))
+    return user
+    */
+  }
+
+  // called when the user clicks the submit button
+  function* registerSubmit() {
+    const valid = yield select(selectors.form.authRegister.valid)
+    if(!valid) {
+      // touch all values of the form so the errors appear
+      yield put(actions.forms.touchAll('authRegister'))
+      return
+    }
+    const credentials = yield select(selectors.form.authRegister.values)
+    yield call(login, credentials)
+  }
+
+  function* logout() {
+    deleteCachedCredentials()
+    yield put(actions.value.set('user', null))
+    yield put(actions.router.redirect('/dashboard'))
+  }
+
+  function* loginSuccess(user) {
+    yield put(actions.router.redirect('/dashboard'))
+  }
+
+  function* registerSuccess(user) {
+    //yield put(actions.router.redirect('/dashboard'))
+    alert('registered')
+  }
+
+  // control access to routes based on the user
+  // if the route (or any parent route) has a 'user={true,false}'
+  function* authenticateRoute() {
+    const userSetting = yield select(state => selectors.router.firstValue(state, 'user'))
+    const redirectSetting = yield select(state => selectors.router.firstValue(state, 'authRedirect'))
+    // this route has no opinion about the user
+    if(typeof(userSetting) != 'boolean') return
+    const user = yield select(state => selectors.valueSelector(state, 'user'))
+    const hasUser = user ? true : false
+    const isRouteAuthenticated = hasUser == userSetting
+    if(!isRouteAuthenticated) {
+      yield put(actions.router.redirect(redirectSetting || '/'))
+    }
+  }
+
+  ///////////////////////////////////////
+  ///////////////////////////////////////
+  // utils
+
+  function* formValuesIfValid(name) {
+    const valid = yield select(selectors.form[name].valid)
+    if(!valid) {
+      // touch all values of the form so the errors appear
+      yield put(actions.forms.touchAll(name))
+      return
+    }
+    const credentials = yield select(selectors.form[name].values)
+    return credentials
+  }
+
+
+
+  return {
+    initialize,
+    logout,
+    status,
+    login,
+    register,
+    loginSuccess,
+    registerSuccess,
+    authenticateRoute
+  }
+}
+
+export default AuthSagas
