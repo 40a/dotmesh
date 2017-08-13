cc227760a7a818eebfb4e8210183a6b66ed50581
Kai Davenport <kaiyadavenport@gmail.com>
2017-08-13 15:00:11 +0100

    clean up layout

--- frontend/src/ui/api/auth/index.js
+++
@@ -1,4 +0,0 @@
-// here is where to swap out the auth api implementation
-import basicAuth from './basic'
-const auth = basicAuth
-export default auth
--- frontend/src/ui/sagas/auth/index.js
+++
@@ -1,4 +0,0 @@
-// here is where to swap out the auth saga implementation
-import basicAuth from './basic'
-const auth = basicAuth
-export default auth
