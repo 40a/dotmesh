9088cdb36966fa93bcfc0a641a13f6825135a4d3
Kai Davenport <kaiyadavenport@gmail.com>
2017-08-13 15:03:55 +0100

    allow headers / query params to be injected

---
+++ frontend/src/ui/api/jsonrpc.js
@@ -0,0 +1,54 @@
+import { request } from 'template-ui/lib/utils/ajax'
+import config from '../../config'
+
+const noop = () => ({})
+
+//
+// * getHeaders - a function that given the payload and state will return headers to inject to each request
+// * getParams - a function that given the payload and state will return query params to inject to each request
+//
+//
+//
+// the returned executor is a function that accepts (payload, state):
+//
+//  * payload
+//    * method
+//    * params
+
+const RPCExecutor = (opts = {}) => {
+  let requestCounter = 0
+  const getHeaders = opts.getHeaders || noop
+  const getParams = opts.getParams || noop
+  const rpc = (payload = {}, state) => {
+    if(!opts.method) throw new Error('method needed')
+
+    const id = requestCounter++
+    const method = opts.method
+    const params = opts.params || {}
+
+    const httpHeaders = getHeaders(payload, state)
+    const httpParams = getParams(payload, state)
+
+    return request({
+      method: 'post',
+      url: config.rpcUrl,
+      headers,
+      params: httpParams,
+      data: {
+        jsonrpc: '2.0',
+        id,
+        method: [config.rpcNamespace, method].join('.'),
+        params
+      }
+    })
+    .then(data => {
+      if(data.id != id) throw new Error(`request id ${id} does not match response id ${data.id}`)
+      if(!data.result) throw new Error(`no result found in response`)
+      return data.result
+    })
+  }
+  return rpc
+}
+
+
+export default RPCExecutor
