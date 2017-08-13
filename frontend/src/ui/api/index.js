72a04cb150c06fa812252293fefeee6919958199
Kai Davenport <kaiyadavenport@gmail.com>
2017-08-13 15:01:39 +0100

    allow composition of api parts not inheritance

--- frontend/src/ui/api/index.js
+++ frontend/src/ui/api/index.js
@@ -1,17 +1,44 @@
 import processLoaders from 'template-ui/lib/plugins/api/processLoaders'
-import auth from './auth'
-import volume from './volume'
+
+import * as selectors from '../selectors'
+import JsonRpc from './jsonrpc'
+
+// switch which type of auth driver we are using here
+import auth from './auth/basic'
+import volumeList from './volume/list'
+
+// a HTTP Basic auth version of the JSONRPC connector
+// you can switch out this connector / use multiple connectors
+// by creating multiple wrappers (see below)
+const connector = JsonRpc({
+  // use the auth driver to inject the current credentials
+  getHeaders: (payload, state) => auth.getHeaders(selectors.auth.user(state)),
+  // we don't want the browser popping up annoying auth windows
+  // the backend golang server is setup for this param name
+  getParams: (payload, state) => ({
+    disableBasicAuthWindow: 'y'
+  })
+})
+
+// wrap an pure api call (that returns an object)
+// with the connector that returns a promise
+// the connector is passed the state so it can inject auth credentials
+const wrapper = (handler) => (payload, state) => connector(handler(payload), state)
 
 // a combo of handler, actions and saga
 const loaders = {
   authLogin: {
-    handler: auth.login,
+    handler: wrapper(auth.login),
+    // these are options passed to the apiSaga runner not the api
     options: {
       processError: (error) => 'incorrect details'
     }
   },
+  // register is not wrapped - it does not need auth and works with ajax not rpc
   authRegister: auth.register,
-  volumeList: volume.list
+
+  // all these methods need to be wrapped - they need auth
+  volumeList: wrapper(volume.list)
 }
 
 const processedLoaders = processLoaders(loaders)
