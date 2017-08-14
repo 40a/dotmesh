ccb0ac31a37f0443709aca4b74a697635b78682c
Kai Davenport <kaiyadavenport@gmail.com>
2017-08-15 00:18:13 +0100

    normal ajax request for register

--- frontend/src/ui/api/auth/basic.js
+++ frontend/src/ui/api/auth/basic.js
@@ -7,17 +7,16 @@ export const login = (payload) => ({
 })
 
 export const register = (payload) => {
-  return new Promise(resolve => {
-    throw new Error('this is a test')
+  return request({
+    method: 'post',
+    url: '/register',
+    data: payload
   })
 }
 
 export const encodeCredentials = (username, password) => new Buffer(username + ':' + password).toString('base64')
 
 export const getHeaders = (credentials) => {
-  console.log('-------------------------------------------');
-  console.log('encide')
-  console.dir(credentials)
   return {
     Authorization: `Basic ${encodeCredentials(credentials.username, credentials.password)}`
   }
