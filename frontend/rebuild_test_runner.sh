aa70b2aaf67dbef11cb977b2f950f2878d35fe59
Kai Davenport <kaiyadavenport@gmail.com>
2017-08-16 00:35:36 +0100

    job to rebuild the rest runner image

---
+++ frontend/rebuild_test_runner.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -xe
+
+export IMAGE=${IMAGE:=$(hostname).local:80/lukemarsden/datamesh-frontend-test:pushpull}
+export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+docker build -t ${IMAGE} -f ${DIR}/test/Dockerfile ${DIR}/test
+
+# allow disabling of registry push
+if [ -z "${NO_PUSH}" ]; then
+  docker push ${IMAGE}
+fi
