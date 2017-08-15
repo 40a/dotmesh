9da84003d2177ee00407d84ac27d143429fa0f44
Kai Davenport <kaiyadavenport@gmail.com>
2017-08-15 01:12:59 +0100

    try listing files

--- frontend/cleanbuild.sh
+++ frontend/cleanbuild.sh
@@ -5,10 +5,12 @@ export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 IMAGE=${IMAGE:=$(hostname).local:80/lukemarsden/datamesh-frontend-builder}
 
-mkdir -p target
-docker build -f Dockerfile -t datamesh-frontend-builder .
+ls -la ${DIR}/dist
+
 docker run --rm \
   -v ${DIR}/dist:/app/dist \
   --entrypoint sh \
   datamesh-frontend-builder \
-  -c "rm -rf /app/dist/*"
+  -c "ls -la /app/dist"
+
+exit 1
