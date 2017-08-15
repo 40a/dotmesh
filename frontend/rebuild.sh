4893035d826802a7b995ec73085625f104a3f216
Kai Davenport <kaiyadavenport@gmail.com>
2017-08-15 17:10:35 +0100

    retry the frontend build without volumes

--- frontend/rebuild.sh
+++ frontend/rebuild.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
+
+#
+# the script that builds the frontend html, javascript and css (and images etc)
+# 
+# steps:
+#
+#   * build the image for the builder - datamesh-frontend-builder
+#   * run the build script inside that image - NO volumes because gitlab root permissionsissue
+#   * copy the build folder (dist) using docker cp
+#   * gzip that folder and clean up
+#
+
 set -xe
 
 export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-IMAGE=${IMAGE:=$(hostname).local:80/lukemarsden/datamesh-frontend-builder}
-
 mkdir -p target
 docker build -f Dockerfile -t datamesh-frontend-builder .
-#docker run --rm \
-#  datamesh-frontend-builder \
-#  release
-
-echo "TODO - run the release without volumes (use cp)"
-
+docker rm -f datamesh-frontend-builder || true
+docker run \
+  --name datamesh-frontend-builder \
+  datamesh-frontend-builder \
+  release
+docker cp datamesh-frontend-builder:/app/dist ./dist
+docker rm -f datamesh-frontend-builder || true
+ls -la ./dist
+tar -cf dist.tar ./dist
+gzip dist.tar
+rm -rf ./dist
