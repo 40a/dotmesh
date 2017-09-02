e449a0801dc2469756f2ce1f104669a6d060805e
Luke Marsden <me@lukemarsden.net>
2017-09-02 23:19:27 +0100

    log reasons

--- prime.sh
+++ prime.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 set +xe
 cd ~/kubernetes
-dind/dind-cluster.sh bare prime-images
+#
+# sudo :-(
+# otherwise we get a bunch of stuff like:
+#
+#     chown: changing ownership of '/home/luke/kubernetes/_output/images/kube-build:build-59cac78cf2-5-v1.8.3-2/Dockerfile': Operation not permitted
+#
+sudo EXTRA_DOCKER_ARGS= dind/dind-cluster.sh bare prime-images
 docker rm -f prime-images
 
 cd $GOPATH/src/github.com/lukemarsden/datamesh/cmd/datamesh-server
