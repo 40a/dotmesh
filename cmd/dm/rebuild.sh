022a21d70b12fbe2f9a583d116c05a48156d6c0d
Luke Marsden <me@lukemarsden.net>
2017-09-02 23:33:59 +0100

    remoooove duplication

--- cmd/dm/rebuild.sh
+++ cmd/dm/rebuild.sh
@@ -6,4 +6,5 @@ fi
 export PATH=/usr/local/go/bin:$PATH
 set -xe
 mkdir -p ../../binaries/$1
+echo CGO_ENABLED=0 GOOS=${1,,} godep go build -a -installsuffix cgo -ldflags '-s' -o ../../binaries/$1/dm .
 CGO_ENABLED=0 GOOS=${1,,} godep go build -a -installsuffix cgo -ldflags '-s' -o ../../binaries/$1/dm .
--- rebuild.sh
+++ rebuild.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-cd cmd/dm
-export PATH=/usr/local/go/bin:$PATH
 set -xe
-mkdir -p ../../binaries/Linux && GOOS=linux godep go build -o ../../binaries/Linux/dm .
+cd cmd/dm
+./rebuild.sh
 cd ../..
 cd cmd/datamesh-server
 ./rebuild.sh
