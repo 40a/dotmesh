65766cc03761c661783a5825cd4c778a5949f040
Luke Marsden <me@lukemarsden.net>
2017-09-08 11:02:35 +0100

    belt and braces

--- mark-cleanup.sh
+++ mark-cleanup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -xe
-for X in $(docker ps --format "{{.Names}}"|grep cluster-); do
+for X in $(docker ps --format "{{.Names}}" | grep cluster- || true); do
     docker exec -ti $X touch /CLEAN_ME_UP
 done
