0d862e16f2defc2e70758f1b0fe046df04f76be6
Luke Marsden <me@lukemarsden.net>
2017-09-26 10:07:25 +0100

    Update image names in prime.sh and its friends

--- prime-docker.sh
+++ prime-docker.sh
@@ -5,9 +5,9 @@ cd $GOPATH/src/github.com/datamesh-io/datamesh/cmd/datamesh-server
 docker build -t $(hostname).local:80/datamesh/datamesh-server:latest .
 docker push $(hostname).local:80/datamesh/datamesh-server:latest
 
-docker pull quay.io/datamesh-io/etcd:v3.0.15
-docker tag quay.io/datamesh-io/etcd:v3.0.15 $(hostname).local:80/datamesh-io/etcd:v3.0.15
-docker push $(hostname).local:80/datamesh-io/etcd:v3.0.15
+docker pull quay.io/datamesh/etcd:v3.0.15
+docker tag quay.io/datamesh/etcd:v3.0.15 $(hostname).local:80/datamesh/etcd:v3.0.15
+docker push $(hostname).local:80/datamesh/etcd:v3.0.15
 
 docker pull busybox
 docker tag busybox $(hostname).local:80/busybox
@@ -18,5 +18,5 @@ docker tag mysql:5.7.17 $(hostname).local:80/mysql:5.7.17
 docker push $(hostname).local:80/mysql:5.7.17
 
 cd ~/datamesh-instrumentation/etcd-browser
-docker build -t $(hostname).local:80/datamesh-io/etcd-browser:v1 .
-docker push $(hostname).local:80/datamesh-io/etcd-browser:v1
+docker build -t $(hostname).local:80/datamesh/etcd-browser:v1 .
+docker push $(hostname).local:80/datamesh/etcd-browser:v1
