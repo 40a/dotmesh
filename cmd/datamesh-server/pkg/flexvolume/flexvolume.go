a53878e2e6463446b49ee80bd1d03c338deb1190
Luke Marsden <me@lukemarsden.net>
2017-10-04 20:17:08 +0100

    Import RPC client, more cnp ;-(

--- cmd/datamesh-server/pkg/flexvolume/flexvolume.go
+++ cmd/datamesh-server/pkg/flexvolume/flexvolume.go
@@ -197,3 +197,44 @@ func main() {
 	driver := NewFlexVolumeDriver()
 	os.Stdout.WriteString(driver.Run(os.Args[1:]))
 }
+
+// TODO read config from /root/.datamesh/config to learn how to auth as admin
+
+// RPC client
+
+func doRPC(hostname, user, apiKey, method string, args interface{}, result interface{}) error {
+	url := fmt.Sprintf("http://%s:6969/rpc", hostname)
+	message, err := json2.EncodeClientRequest(method, args)
+	if err != nil {
+		return err
+	}
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(message))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.SetBasicAuth(user, apiKey)
+	client := new(http.Client)
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		fmt.Printf("Test RPC FAIL: %+v -> %s -> %+v\n", args, method, err)
+		return err
+	}
+
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("Test RPC FAIL: %+v -> %s -> %+v\n", args, method, err)
+		return fmt.Errorf("Error reading body: %s", err)
+	}
+	err = json2.DecodeClientResponse(bytes.NewBuffer(b), &result)
+	if err != nil {
+		fmt.Printf("Test RPC FAIL: %+v -> %s -> %+v\n", args, method, err)
+		return fmt.Errorf("Couldn't decode response '%s': %s", string(b), err)
+	}
+	fmt.Printf("Test RPC: %+v -> %s -> %+v\n", args, method, result)
+	return nil
+}
